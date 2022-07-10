1d7dfe203a59dd9513c700cdbfea78483bb83086
Ezra Casas <ezra.casas@ctemplar.com>
2022-07-09 22:05:51 -0600

    more styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,21 +9,36 @@
         {name: "Jobs", value: "jobs", active: false},
         {name: "Store", value: "store", active: false},
     ]
+    let current = true;
 </script>
 
 <nav>
     <div>
-
+        <h1>Blender Logo</h1>
     </div>
-    {#each navigations as page}
-        <a href="/{page.value}">{page.name}</a>
-    {/each}
+    <div class="nav-items">
+       {#each navigations as page}
+            <a
+                class="{current === true ? 'selected' : ''}"
+                href="/{page.value}">{page.name}</a>
+        {/each}
+    </div>
+
 </nav>
 
 <style>
     nav{
         display: flex;
         justify-content: space-around;
+        align-items: center;
+    }
+    .nav-items{
+        display: flex;
+        justify-content: space-between;
+    }
+    h1{
+        color: lightcoral;
     }
 </style>
+
 <slot></slot>
